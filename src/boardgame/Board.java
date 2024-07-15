e51bdac71fa939101762657f6bfb13eb9bbefd90
alesilvasp <alesilvasp@gmail.com>
2024-07-15 16:13:31 -0300

    create chess layer and printing board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,4 +28,12 @@ public class Board {
         return colunmns;
     }
 
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 }
