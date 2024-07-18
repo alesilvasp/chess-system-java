85424d5014ee696ba09e5ff7a8c07c1f7f9a7a65
alesilvasp <alesilvasp@gmail.com>
2024-07-18 14:52:51 -0300

    Switching player

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -70,7 +71,18 @@ public class UI {
 
     }
 
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        if (chessMatch.getCurrentPlayer() == Color.BLACK) {
 
+            System.out.println("Waiting player: " + ANSI_YELLOW + chessMatch.getCurrentPlayer() + ANSI_RESET);
+        } else {
+            System.out.println("Waiting player: " + ANSI_WHITE + chessMatch.getCurrentPlayer() + ANSI_RESET);
+
+        }
+    }
 
     private static void printPiece(ChessPiece piece, boolean background) {
         if (background) {
